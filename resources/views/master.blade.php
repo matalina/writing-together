16b5cce2b8d82ba9d0f7223446b13130492d244c
Alicia Wilkerson <matalina@gmail.com>
2017-04-08 23:00:32 +0000

    fixed secure url

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="{{ secure_url('css/app.css') }}" />
+        <link rel="stylesheet" href="{{ url('css/app.css') }}" />
         <style>
             
         </style>
